dff42197af8c81fb3e0091a6b176da3f026e7eab
Vladimir <zpoison1993@gmail.com>
2019-02-16 21:46:47 +0300

    slider has been done v.0.1

--- js/main.js
+++ js/main.js
@@ -94,44 +94,51 @@ openMenu();
 //Появление списка состава блюда
 
 
-function openComponents() {
-    const openComponentsBtn = document.querySelector('.composition');
-    const closeComponentsBtn = document.querySelector('.content-close');
-    const components = document.querySelector('.composition-content');
+function openComponents(composition,content) {
+   
+    const openComponentsBtn = document.querySelector(composition);
+    
+    const components = document.querySelector(content);
+    // const closeComponentsBtn = document.querySelector('.content-close');
+    let closeComponentsBtn = components.getElementsByTagName('BUTTON');
+    closeComponentsBtn = closeComponentsBtn[0];
     let op = 0.2;
     let notPushed = true; //Этот флаг говорит нам о том,что Состав блюда не раскрыт
 
-    openComponentsBtn.addEventListener('click', function (event) {
+     
+        openComponentsBtn.addEventListener('click', function (event) {
         
-        //Если Состав блюда не раскрыт - это истина, то открываем
-        if (notPushed) { 
-            components.style.display = 'flex';
-            components.style.opacity = 0.2;
-            setTimeout(function appear() {
-                if (op < 1) {
-                    op += 0.1;
-                    components.style.opacity = op;
-                    setTimeout(appear, 20);
-                }
-            }, 20);
-        }
-        //Иначе - оно было раскрыто, значит, при нажатии нам его надо закрыть
-        else {
-            setTimeout(function close() {
-                if (op > 0) {
-                    op -= 0.1;
-                    components.style.opacity = op;
-                    setTimeout(close, 20);
-                }
-
-                else {
-                    components.style.display = "none";
-                }
-            }, 20);
-        }
-        notPushed = !notPushed; //после завершения операции клика по кнопке. флаг раскрытости "Состава" блюда меняется на противоположное значение
-
-    });
+            //Если Состав блюда не раскрыт - это истина, то открываем
+            if (notPushed) { 
+                components.style.display = 'flex';
+                components.style.opacity = 0.2;
+                setTimeout(function appear() {
+                    if (op < 1) {
+                        op += 0.1;
+                        components.style.opacity = op;
+                        setTimeout(appear, 20);
+                    }
+                }, 20);
+            }
+            //Иначе - оно было раскрыто, значит, при нажатии нам его надо закрыть
+            else {
+                setTimeout(function close() {
+                    if (op > 0) {
+                        op -= 0.1;
+                        components.style.opacity = op;
+                        setTimeout(close, 20);
+                    }
+    
+                    else {
+                        components.style.display = "none";
+                    }
+                }, 20);
+            }
+            notPushed = !notPushed; //после завершения операции клика по кнопке. флаг раскрытости "Состава" блюда меняется на противоположное значение
+    
+        });
+    
+    
 
     closeComponentsBtn.addEventListener('click', function (event) {
         notPushed = true; // для данного варианта закрытия меню - флаг нажатия/ненажатия на основную кнопку "Состав" не требуется
@@ -150,8 +157,51 @@ function openComponents() {
     });
 
 }
+openComponents('#composition1','#content1');
+openComponents('#composition2','#content2');
+openComponents('#composition3','#content3');
+openComponents('#composition4','#content4');
+openComponents('#composition5','#content5');
 
-openComponents();
+
+
+
+//
+//Слайдер лдя бургеров
+const left = document.querySelector('.previous-button');
+const right = document.querySelector('.next-button');
+const items = document.querySelector('.slider-wrapper');
+
+let step = getComputedStyle(items.firstElementChild).width;
+step = parseInt(step);
+let slidesInView = 1; // 1 -1 слайд помещается за раз в слайдер
+const maxRight = (items.children.length - slidesInView)*step;
+const minRight = 0;
+let currentRight = 0;
+
+right.addEventListener('click', function(event){
+    event.preventDefault;
+    if(currentRight<maxRight)   {
+        currentRight +=step;
+        items.style.right = `${currentRight}px`;
+    }
+    else {
+        currentRight=0;
+        items.style.right = 0;
+    }
+});
+
+left.addEventListener('click',function(event){
+    event.preventDefault;
+    if(currentRight>minRight)   {
+        currentRight -= step;
+        items.style.right = `${currentRight}px`;
+    } else {
+        currentRight = maxRight;
+        items.style.right = maxRight;
+    }
+});
+
 
 
 //
@@ -199,3 +249,54 @@ function menusOpen() {
 }
 
 menusOpen();
+
+
+
+//
+//Модальные окна в отзывах
+    const buttonsMore = document.querySelectorAll('.more');
+    const popup = document.querySelector('.reviews-popup');
+    const closePopup = document.querySelector('.content-close');
+    console.log(closePopup);
+    
+    for( var buttonMore of buttonsMore) {
+        console.log(buttonMore);
+        buttonMore.addEventListener('click',function(event){
+        console.log(buttonMore);
+            popup.classList.add('opened');
+    })
+
+     
+    }
+   
+    closePopup.addEventListener('click',function(event) {
+        popup.style.left = 9999 + 'px';
+    })
+
+
+//
+//Работа формы заказа
+
+const myForm = document.querySelector('.form');
+const send = document.querySelector('#send');
+
+send.addEventListener('click',function(event) {
+    event.preventDefault();
+
+    const FormData = {
+        name: myForm.elements.name.value,
+        phone: myForm.elements.phone.value,
+        comment: myForm.elements.comment.value
+    }
+   
+
+    const xhr = new XMLHttpRequest();
+    xhr.responseType='json';
+    xhr.open('POST','https://webdev-api.loftschool.com/sendmail');
+    xhr.send(JSON.stringify(FormData));
+
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response);
+    });
+
+})
